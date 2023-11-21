63c435a39202600b9b015617a297c4cf87bfbf3e
Julie Cariou <julie.cariou2605@gmail.com>
2023-11-21 14:58:05 +0100

    Code JS

---
+++ main.js
@@ -0,0 +1,47 @@
+//Récupérer données API
+function showListPokemon(list) {
+    list.forEach(element => {
+        showPokemon(element)
+    });
+}
+
+function showPokemon(pokemon) {
+
+    //Cibler
+    let cible = document.getElementById("listPokemon");
+
+    //Cibler name
+    let name = JSON.stringify(pokemon.name);
+
+    console.log(name);
+
+    let objectName = JSON.parse(name);
+
+    let PokeName = objectName["fr"];
+
+    //Cibler Sprites
+    let sprites = JSON.stringify(pokemon.sprites);
+
+    let objectSprites = JSON.parse(sprites);
+
+    let PokeSprites = objectSprites["regular"];
+
+    //Créer le contenu
+    let contenu = `
+    <p class="Pokemon"> n°`+ pokemon.pokedexId +`
+    </br>`+ pokemon.generation +`e Génération
+    </br> Nom :`+ PokeName +`</p>
+    </br>
+    <img
+    class="imgPoke"
+    src="`+ PokeSprites +`"
+    alt="Image de `+ PokeName +`" />`;
+
+    //Ajouter le contenu dans la cible
+    cible.innerHTML = cible.innerHTML + contenu;
+
+}
+
+fetch('https://tyradex.vercel.app/api/v1/pokemon')
+.then((response) => response.json())
+.then((listPokemon) => showListPokemon(listPokemon));
