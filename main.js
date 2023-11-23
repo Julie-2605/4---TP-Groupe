068a0dbbb125a053baae484ce891998ba6f29abb
Julie Cariou <julie.cariou2605@gmail.com>
2023-11-23 16:06:45 +0100

    JS Function

--- main.js
+++ main.js
@@ -1,10 +1,29 @@
-//Récupérer données API
-function showListPokemon(list) {
+//LISTE GENERATION
+function showListGeneration(list) {
     list.forEach(element => {
-        showPokemon(element)
+        showGeneration(element)
     });
 }
 
+
+//AFFICHAGE DES BUTTONS GÉNÉRATIONS
+function showGeneration(Generation) {
+    let cible = document.getElementById("Generation");
+
+    let contenu = `
+    <button class="buttonGeneration" type="button" onclick="returnGeneration(`+ Generation.generation + `)"> Génération ` + Generation.generation + `</button>
+    `;
+
+    cible.innerHTML = cible.innerHTML + contenu;
+}
+
+//Récupérer données API POUR AFFICHER LES BUTTONS GÉNÉRATION
+fetch('https://tyradex.vercel.app/api/v1/gen')
+    .then((response) => response.json())
+    .then((listPokemon) => showListGeneration(listPokemon));
+
+
+//LISTE DE POKEMON
 function showPokemon(pokemon) {
 
     //Cibler
@@ -12,8 +31,6 @@ function showPokemon(pokemon) {
 
     //Cibler name
     let name = JSON.stringify(pokemon.name);
-
-    console.log(name);
 
     let objectName = JSON.parse(name);
 
@@ -26,22 +43,160 @@ function showPokemon(pokemon) {
 
     let PokeSprites = objectSprites["regular"];
 
+    //Cibler Types
+    let types = JSON.stringify(pokemon.types);
+
+    let objectTypes = JSON.parse(types);
+
+    console.log(objectTypes);
+
+    // for (let i = 0; i >= objectTypes.length; i++) {
+    //     let PokeTypes = PokeTypes + objectTypes[i]["name"];
+    //     return PokeTypes;
+    // }
+
+
     //Créer le contenu
     let contenu = `
-    <p class="Pokemon"> n°`+ pokemon.pokedexId +`
-    </br>`+ pokemon.generation +`e Génération
-    </br> Nom :`+ PokeName +`</p>
-    </br>
-    <img
-    class="imgPoke"
-    src="`+ PokeSprites +`"
-    alt="Image de `+ PokeName +`" />`;
+        <article class="PokemonCard" onclick="showPokeDetails(`+ pokemon.pokedexId + `)">
+            <p> n°`+ pokemon.pokedexId + `
+            </br> Nom : `+ PokeName + `
+            </p>
+            </br>
+            <img
+            class="imgPoke"
+            src="`+ PokeSprites + `"
+            alt="Image de `+ PokeName + `" />
+        </article>`;
 
     //Ajouter le contenu dans la cible
     cible.innerHTML = cible.innerHTML + contenu;
 
 }
 
-fetch('https://tyradex.vercel.app/api/v1/pokemon')
-.then((response) => response.json())
-.then((listPokemon) => showListPokemon(listPokemon));
+//RETOUR DU NUMÉRO DE LA GÉNÉRATION
+function returnGeneration(NumberGen) {
+
+    //LISTE POKEMON PAR GENERATION
+    function showListPokemon(list) {
+
+        list.forEach(element => {
+            showPokemon(element)
+        });
+
+    }
+
+    //Récupérer données API
+    fetch('https://tyradex.vercel.app/api/v1/gen/' + NumberGen)
+        .then((response) => response.json())
+        .then((listPokemon) => showListPokemon(listPokemon));
+}
+
+//POKEMON DETAILS
+
+async function showPokeDetails(PokeID) {
+
+    let response = await fetch('https://tyradex.vercel.app/api/v1/pokemon/' + PokeID);
+    let pokemon = await response.json();
+
+    //Cibler name
+    let name = JSON.stringify(pokemon.name);
+
+    let objectName = JSON.parse(name);
+
+    let PokeName = objectName["fr"];
+
+    //Cibler Sprites
+    let sprites = JSON.stringify(pokemon.sprites);
+
+    let objectSprites = JSON.parse(sprites);
+
+    let PokeSprites = objectSprites["regular"];
+
+    //Cibler Types
+    let types = JSON.stringify(pokemon.types);
+
+    let objectTypes = JSON.parse(types);
+
+    console.log(objectTypes);
+
+    let PokeTypes = objectTypes[0]["name"];
+
+    let Evolution = JSON.stringify(pokemon.evolution);
+
+    console.log(Evolution);
+
+    let objectEvolution = JSON.parse(Evolution);
+
+    console.log(objectEvolution);
+
+    let PokeEvolution = objectEvolution["next"]["pokedexId"];
+
+
+    let contenu = `
+        <article>
+        <p>n°`+ pokemon.pokedexId + `
+        </br>`+ pokemon.generation + `e Génération
+        </br> Nom : `+ PokeName + `
+        </br> Type : `+ PokeTypes + `
+        </p>
+        </br>
+        <img
+        class="imgPoke"
+        src="`+ PokeSprites + `"
+        alt="Image de `+ PokeName + `" />
+        </article>
+        <button class="Evolutions" type="button" onclick="EvolutionAnt()"> < </button>
+        <button class="Evolutions" type="button" onclick="EvolutionPrec()"> > </button>
+        </br>
+        <button class="goBack" type="button" onclick="goBack()">Retourner à la liste</button>
+        <p>TEST :`+ PokeEvolution + `</p>`;
+
+    $("#PokeDetails").html(contenu);
+    $("#listPokemon").hide();
+    $("#PokeDetails").show();
+}
+//REVENIR EN ARRIÈRE SUR LA LISTE
+function goBack() {
+    $("#PokeDetails").hide();
+    $("#listPokemon").show();
+}
+
+
+
+
+//POKEMON RANDOM
+async function getRandomPokemon() {
+    let response = await fetch('https://tyradex.vercel.app/api/v1/pokemon');
+    let listAllPokemon = await response.json();
+
+    let RandomId = Math.floor(Math.random() * listAllPokemon.length) + 1;
+
+    let PokeRandom = listAllPokemon[RandomId];
+    console.log(PokeRandom);
+
+    return PokeRandom;
+
+}
+
+getRandomPokemon()
+    .then((PokeRandom) => {
+        let contenu = showPokemon(PokeRandom);
+        $("#PokeRandom").html(contenu);
+    });
+
+
+
+//LISTE PAR TYPE
+function showListTypes(list) {
+    list.forEach(element => {
+        showTypes(element)
+    });
+}
+
+//TYPES DETAILS
+
+function showListDetails() {
+
+}
+
